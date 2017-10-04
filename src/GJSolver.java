d001dca4c9880e71057c5c55214ff267e9d84619
nyxia91 <danielchristian0711@Gmail.com>
2017-10-04 23:44:45 +0700

    nyoba commit, semoga ga malah jdi error wkwkkwkw

--- src/GJSolver.java
+++ src/GJSolver.java
@@ -2,7 +2,9 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.Scanner;
 import java.util.ArrayList;
-
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+        
 public class GJSolver {
 	private static GJSolver instance;
 
@@ -241,7 +243,53 @@ public class GJSolver {
 		Matrix tempMatrix = new Matrix(getReducedEchelon(mx));
 		return tempMatrix;
 	}
-
+        
+        public void interpolasi_linier()
+        {
+             Scanner in = new Scanner(System.in);
+          System.out.print("Banyak data : ");
+          int N = in.nextInt();
+          double dataX[] = new double[N];
+          double dataY[] = new double[N];
+          //input data x & y
+          for(int i=0; i<N; i++){
+               System.out.print("X"+(i+1)+" = ");
+               dataX[i]=in.nextDouble();
+               System.out.print("Y"+(i+1)+" = ");
+               dataY[i]=in.nextDouble();
+          }
+          //mencetak data x & y
+          System.out.print("\tX |");
+          for(int i=0; i<N; i++){
+               System.out.print(" "+dataX[i]+" |");
+          }
+          System.out.print("\n\tY |");
+               for(int i=0; i<N; i++){
+               System.out.print(" "+dataY[i]+" |");
+          }
+          //yang dicari
+          System.out.println();
+          System.out.print("\nTentukan Nilai X = ");
+          double x=in.nextDouble();
+          double x1=0,x2=0,y1=0,y2=0;
+          //penyeleksian
+          for(int i=0; i<N-1; i++){
+               if(x>dataX[i] && x<dataX[i+1]){
+               x1=dataX[i];
+               x2=dataX[i+1];
+               y1=dataY[i];
+               y2=dataY[i+1];
+               }
+          }
+          System.out.println("x1 = "+x1);
+          System.out.println("x2 = "+x2);
+          System.out.println("y1 = "+y1);
+          System.out.println("y2 = "+y2);
+          double y=y1+((y2-y1)/(x2-x1))*(x-x1);
+          System.out.println("Titik terbaru adalah P3("+x+","+y+")");
+        }
+        
+                
 	public static void main (String[] args) {
 		Matrix mx = new Matrix("myMatrix3.txt");
                 
